6a08e841fc12dd773c47e7c95af75f61e2bcdb2f
saadbenchekroun <148226123+saadbenchekroun@users.noreply.github.com>
2024-09-18 12:14:25 +0100

    Update

--- optimization.m
+++ optimization.m
@@ -1,13 +1,23 @@
-function u=newtonappro(x0,nmax)
+function u = optimization(x0, nmax)
+    % optimization: Uses Newton's method to optimize machine operational parameters
+    % Input: 
+    %   x0 - Initial guess for the optimal value (e.g., pressure or temperature)
+    %   nmax - Maximum number of iterations for convergence
+    % Output:
+    %   u - Optimized value for the machine parameter
 
-f=@(x) (x+1).*(x-3./4);
-fprime=@(x) 2.*x+1./4;
+    % Example machine performance function and its derivative
+    f = @(x) (x + 1) .* (x - 3 / 4);  % Function representing machine performance
+    fprime = @(x) 2 .* x + 1 / 4;     % Derivative of the performance function
 
-y=zeros(nmax);
-y(1)=x0;
-for i=1:nmax-1
-    y(i+1)=y(i)-(f(y(i))/fprime(y(i)));
-end
-u=y(nmax);
-fprintf('La valeur approchee de la racine est :\n')
+    y = zeros(1, nmax);  % Initialize an array to store iterates
+    y(1) = x0;           % Start with the initial guess
+
+    % Newton's method iterations
+    for i = 1:nmax-1
+        y(i+1) = y(i) - (f(y(i)) / fprime(y(i)));  % Update the guess using Newton's method
+    end
+
+    u = y(nmax);  % The final optimized value after nmax iterations
+    fprintf('The optimized parameter value is: %f\n', u);
 end
